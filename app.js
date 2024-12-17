8d22b00fa8afc76492c42ac87dc3cc214ff62668
busratok <busratokuc@gmail.com>
2024-12-17 12:09:44 +0000

    submenu is added

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const navToggle = document.querySelector(".nav__toggle");
 const list = document.querySelector(".nav__list");
 
+// Main Menu Toggle
 navToggle.addEventListener("click", (e) => {
   console.log(list.classList);
   e.target.classList.toggle("nav__toggle--rotate");
   list.classList.toggle("nav__show--list");
+});
+
+// Submenu Toggle
+const submenus = document.querySelectorAll(".nav__item--has-submenu");
+
+submenus.forEach((submenu) => {
+  submenu.addEventListener("click", (e) => {
+    const childSubmenu = submenu.querySelector(".nav__submenu");
+    if (childSubmenu) {
+      childSubmenu.classList.toggle("nav__submenu--show");
+    }
+  });
 });
